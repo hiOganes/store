781958a38af2bb9a3f70bc8f67ea6cd4f5dc511b
hiOganes <soft.oganes@gmail.com>
2025-09-11 19:02:39 +0300

    update .env

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*s2ci6ga-==7c*m2qkway+r0up1cj(d%14*1dn9k5mkoq4*g8c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0']
 
